99eb8d8b5e67d49c9eb77a749e41314ee1ee36df
Evgeniy <evgeniyy-gorbunenko@mail.ru>
2021-05-15 13:47:11 +0300

    google auth

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    HWI\Bundle\OAuthBundle\HWIOAuthBundle::class => ['all' => true],
+    Http\HttplugBundle\HttplugBundle::class => ['all' => true],
 ];
